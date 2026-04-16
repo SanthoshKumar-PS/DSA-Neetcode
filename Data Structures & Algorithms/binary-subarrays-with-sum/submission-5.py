16c7c00cbb68c6ab2a30d138587e5ee079ceb97b
Santhosh Kumar <161190692+SanthoshKumar-PS@users.noreply.github.com>
2026-04-16 23:05:13 +0530

    Add: binary-subarrays-with-sum - submission-5

---
+++ Data Structures & Algorithms/binary-subarrays-with-sum/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        def atMost(x):
+            res = count = l = 0
+            for r in range(len(nums)):
+                count+=nums[r]
+                while count>x and l<=r:
+                    count-=nums[l]
+                    l+=1
+                res+=(r-l+1)
+            return res
+        return atMost(goal) - atMost(goal-1)
